cf6ceca72990cdff5b01c123280de9724145a8d7
srushti611 <srushtijsundar@gmail.com>
2026-06-27 12:32:20 +0530

    Update port bindings for cloud deployment

--- server-app/index.js
+++ server-app/index.js
@@ -110,4 +110,5 @@ app.post('/api/verify-code', async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("🚀 Major Project Core API running live on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Major Project Core API running live on port ${PORT}`));
